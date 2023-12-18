3ffae5be68c3a720e736c8f32b9c72c1067c6340
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-12-18 09:49:45 -0300

    refactor(wordpress): simplify entrypoint

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-wget "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+shopt -s expand_aliases
+alias wp='wp --allow-root'
 
-wp core download --locale="pt_BR" --allow-root
-wp language core activate "pt_BR" --allow-root
+wp core download --locale="pt_BR"
+wp language core activate "pt_BR"
 
-wp config create --allow-root \
+wp config create \
   --dbname="$WP_DATABASE" \
   --dbuser="$WP_USER" \
   --dbpass="$WP_PASSWORD" \
@@ -20,17 +19,17 @@ wp core install \
   --admin_user="$WP_USER" \
   --admin_password="$WP_PASSWORD"  \
   --admin_email="$WP_USER@email.com" \
-  --skip-email --allow-root
+  --skip-email
 
 wp user create marcelo marcelo@email.com \
   --user_pass=wpass \
   --role=subscriber \
-  --porcelain --allow-root
+  --porcelain
 
-wp plugin install redis-cache --activate --allow-root
-wp config set WP_REDIS_HOST "redis" --allow-root
-wp config set WP_REDIS_PORT 6379 --allow-root
-wp config set WP_CACHE true --allow-root
-wp redis enable --allow-root
+wp plugin install redis-cache --activate
+wp config set WP_REDIS_HOST "redis"
+wp config set WP_REDIS_PORT 6379
+wp config set WP_CACHE true
+wp redis enable
 
 php-fpm -F
