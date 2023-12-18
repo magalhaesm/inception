cd550f4583735a270e18cc6d8137c78044c3337c
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-12-18 09:28:02 -0300

    feat(bonus): add redis cache

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,17 +2,17 @@
 
 wget "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
 chmod +x wp-cli.phar
-mv wp-cli.phar /usr/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --allow-root
-wp language core install "pt_BR" --activate --allow-root
+wp core download --locale="pt_BR" --allow-root
+wp language core activate "pt_BR" --allow-root
 
 wp config create --allow-root \
   --dbname="$WP_DATABASE" \
   --dbuser="$WP_USER" \
   --dbpass="$WP_PASSWORD" \
   --dbhost=mariadb \
-  --dbprefix="wp_" \
+  --dbprefix="wp_"
 
 wp core install \
   --url=localhost \
@@ -26,5 +26,11 @@ wp user create marcelo marcelo@email.com \
   --user_pass=wpass \
   --role=subscriber \
   --porcelain --allow-root
+
+wp plugin install redis-cache --activate --allow-root
+wp config set WP_REDIS_HOST "redis" --allow-root
+wp config set WP_REDIS_PORT 6379 --allow-root
+wp config set WP_CACHE true --allow-root
+wp redis enable --allow-root
 
 php-fpm -F
