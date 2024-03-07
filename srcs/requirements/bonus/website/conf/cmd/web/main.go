2964af7c54d043481e88f0540fa2561b4e41cee2
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2024-03-07 17:36:05 -0300

    website: fix layout

--- srcs/requirements/bonus/website/conf/cmd/web/main.go
+++ srcs/requirements/bonus/website/conf/cmd/web/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
-// TODO: anexa ou não /website/
 func main() {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,4 +15,20 @@ func main() {
 	log.Println("Starting server on :8080")
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	ts, err := template.ParseFiles("./ui/static/index.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
